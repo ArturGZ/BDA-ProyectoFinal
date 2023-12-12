7a1c1e7282b727da71368956c886b0c595cf4492
Miguel Arroyo <miguelarroyo217@gmail.com>
2023-12-11 21:48:41 -0600

    Script para crear loop devices y carpetas del proyecto

---
+++ Scripts/s-01-crea-carpetas-loop-devices-root.sh
@@ -0,0 +1,65 @@
+# @Autores       J. Miguel Arroyo Quiroz, Arturo Perez Quintana
+# @Fecha         11/12/2023
+# @Descripcion   Creación de loop devices y carpetas empleadas en el proyecto final de BDA 2024-1
+
+echo "Creando directorio para disk-images"
+mkdir /unam-bda/ProyectoFinal
+mkdir /unam-bda/ProyectoFinal/disk-images
+
+echo "Ingresando al directorio creado"
+cd /unam-bda/ProyectoFinal/disk-images
+
+echo "Creando archivos binarios"
+#diskProy1.img
+dd if=/dev/zero of=diskProy1.img bs=100M count=10
+#diskProy2.img
+dd if=/dev/zero of=diskProy2.img bs=100M count=10
+#diskProy3.img
+dd if=/dev/zero of=diskProy3.img bs=100M count=10
+
+echo "Comprobando su creación"
+du -sh diskProy*.img
+
+echo "Creando loop device para cada disk-image"
+losetup -fP diskProy1.img
+losetup -fP diskProy2.img
+losetup -fP diskProy3.img
+
+echo "Confirmando la creación de los loop devices"
+losetup -a
+
+echo "Dando formato ext4 a los nuevos dispositivos"
+mkfs.ext4 diskProy1.img
+mkfs.ext4 diskProy2.img
+mkfs.ext4 diskProy3.img
+
+echo "Creando los directorios donde los dispositivos serán montados"
+#Redo Log 1
+mkdir /unam-bda/ProyectoFinal/d01
+#Redo Log 2
+mkdir /unam-bda/ProyectoFinal/d02
+#Redo Log 3
+mkdir /unam-bda/ProyectoFinal/d03
+
+#Agregar las siguientes líneas al final del archivo /etc/fstab
+#/unam-bda/ProyectoFinal/disk-images/diskProy1.img /unam-bda/ProyectoFinal/d01 auto loop 0 0
+#/unam-bda/ProyectoFinal/disk-images/diskProy2.img /unam-bda/ProyectoFinal/d02 auto loop 0 0
+#/unam-bda/ProyectoFinal/disk-images/diskProy3.img /unam-bda/ProyectoFinal/d03 auto loop 0 0
+
+echo "Creando el resto de directorios empleados en el proyecto"
+#Data Files 1
+mkdir /unam-bda/ProyectoFinal/d04
+#Data Files 2
+mkdir /unam-bda/ProyectoFinal/d05
+#Data Files 3
+mkdir /unam-bda/ProyectoFinal/d06
+#Data File Elementos Tipo Blob
+mkdir /unam-bda/ProyectoFinal/d07
+#Archived Redo Log 1
+mkdir /unam-bda/ProyectoFinal/d08
+#Archived Redo Log 2
+mkdir /unam-bda/ProyectoFinal/d09
+#Archived Redo Log 3
+mkdir /unam-bda/ProyectoFinal/d10
+#FRA
+mkdir /unam-bda/ProyectoFinal/d11
