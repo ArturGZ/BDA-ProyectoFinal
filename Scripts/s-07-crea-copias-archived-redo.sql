be28826b8a29771930d76f5ba5d259b41ba628ad
ArturGZ <arturo.quintana.016@gmail.com>
2023-12-12 16:04:48 -0600

    Agrega creacion de 2 copias para archived redo logs

--- Scripts/s-07-crea-copias-archived-redo.sql
+++ Scripts/s-07-crea-copias-archived-redo.sql
@@ -1,25 +1,18 @@
 -- @Autores       J. Miguel Arroyo Quiroz, Arturo Perez Quintana
 -- @Fecha         12/12/2023
--- @Descripcion   Creación de copias adicionales de archived redo logs
+-- @Descripcion   Creación de copias adicionales de archived redo logs y control files
 
 whenever sqlerror exit rollback;
 connect sys as sysdba;
 
 
-Prompt Se configuran 2 copias adicionales a la FRA en sus respectivos discos
+Prompt Se crean 2 copias adicionales a la FRA en sus respectivos discos
 alter system set log_archive_dest_2='LOCATION=/unam-bda/ProyectoFinal/d06/app/oradata/ARPEPROY/' scope=spfile;
 alter system set log_archive_dest_3='LOCATION=/unam-bda/ProyectoFinal/d07/app/oradata/ARPEPROY/' scope=spfile;
 
-
-Prompt Deteniendo la instancia
+Prompt Reiniciando la instancia
 shutdown immediate;
-
-
-Prompt Abriendo la base en modo archived
-startup mount
-alter database archivelog;
-alter database open;
-
+shutdown open;
 
 Prompt Comprobando que se haya habilitado el modo archivelog
 archive log list
