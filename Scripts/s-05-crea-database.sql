a13236baa167910fbec2bda3650f8722066d1251
Miguel Arroyo <miguelarroyo217@gmail.com>
2023-12-12 18:43:24 -0600

    Modificación de rutas Create-Database

--- Scripts/s-05-crea-database.sql
+++ Scripts/s-05-crea-database.sql
@@ -12,31 +12,31 @@ create database arpeproy
     user system identified by arpeproy1
     logfile group 1(
         '',
-        '/unam-bda/ProyectoFinal/d01/app/oradata/ARPEPROY/redo01b.log',
-        '/unam-bda/ProyectoFinal/d02/app/oradata/ARPEPROY/redo01c.log') size 100m blocksize 512
+        '/unam-bda/ProyectoFinal/d01/ARPEPROY/redo01b.log',
+        '/unam-bda/ProyectoFinal/d02/ARPEPROY/redo01c.log') size 100m blocksize 512
     group 2 (
         '',
-        '/unam-bda/ProyectoFinal/d01/app/oradata/ARPEPROY/redo02b.log',
-        '/unam-bda/ProyectoFinal/d02/app/oradata/ARPEPROY/redo02c.log') size 100m blocksize 512
+        '/unam-bda/ProyectoFinal/d01/ARPEPROY/redo02b.log',
+        '/unam-bda/ProyectoFinal/d02/ARPEPROY/redo02c.log') size 100m blocksize 512
     group 3(
         '',
-        '/unam-bda/ProyectoFinal/d01/app/oradata/ARPEPROY/redo03b.log',
-        '/unam-bda/ProyectoFinal/d02/app/oradata/ARPEPROY/redo03c.log') size 100m blocksize 512
+        '/unam-bda/ProyectoFinal/d01/ARPEPROY/redo03b.log',
+        '/unam-bda/ProyectoFinal/d02/ARPEPROY/redo03c.log') size 100m blocksize 512
     maxlogfiles 9
     maxlogs members 3
     maxdatafile 1024
     character set AL32UTF8
     extent management local
-    datafile '/unam-bda/ProyectoFinal/d03/app/oradata/ARPEPROY/system01.dbf'
+    datafile '/unam-bda/ProyectoFinal/d03/ARPEPROY/system01.dbf'
         size 700m reuse autoextend on next 1024k maxsize unlimited
-    sysaux datafile '/unam-bda/ProyectoFinal/d03/app/oradata/ARPEPROY/sysaux01.dbf'
+    sysaux datafile '/unam-bda/ProyectoFinal/d03/ARPEPROY/sysaux01.dbf'
         size 550m reuse autoextend on next 1024k maxsize unlimited
     default tablespace users
-        datafile '/unam-bda/ProyectoFinal/d04/app/oradata/ARPEPROY/users01.dbf'
+        datafile '/unam-bda/ProyectoFinal/d04/ARPEPROY/users01.dbf'
         size 500m reuse autoextend on maxsize unlimited
     default temporary tablespace tempts1
-        tempfile '/unam-bda/ProyectoFinal/d03/app/oradata/ARPEPROY/temp01.dbf'
+        tempfile '/unam-bda/ProyectoFinal/d03/ARPEPROY/temp01.dbf'
         size 20m reuse autoextend on next 640k maxsize unlimited
     undo tablespace undots1
-        datafile '/unam-bda/ProyectoFinal/d03/app/oradata/ARPEPROY/undots01.dbf'
-        size 200m reuse autoextend on next 512k maxsize unlimited
+        datafile '/unam-bda/ProyectoFinal/d03/ARPEPROY/undots01.dbf'
+        size 200m reuse autoextend on next 512k maxsize unlimited;
