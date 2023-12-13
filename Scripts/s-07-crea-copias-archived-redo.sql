e9024279f467607a8440de4514469deb106cdb21
ArturGZ <arturo.quintana.016@gmail.com>
2023-12-13 01:08:02 -0600

    Script para multiplexar archived redo

--- Scripts/s-07-crea-copias-archived-redo.sql
+++ Scripts/s-07-crea-copias-archived-redo.sql
@@ -5,14 +5,17 @@
 whenever sqlerror exit rollback;
 connect sys as sysdba;
 
-
 Prompt Se crean 2 copias adicionales de archived redo logs a la FRA en sus respectivos discos
 alter system set log_archive_dest_2='LOCATION=/unam-bda/ProyectoFinal/d06/ARPEPROY' scope=spfile;
 alter system set log_archive_dest_3='LOCATION=/unam-bda/ProyectoFinal/d07/ARPEPROY' scope=spfile;
 
-Prompt Reiniciando la instancia
+Prompt Deteniendo la instancia
 shutdown immediate;
-shutdown open;
+
+Prompt Abriendo la base en modo archived
+startup mount
+alter database archivelog;
+alter database open;
 
 Prompt Comprobando que se haya habilitado el modo archivelog
 archive log list
